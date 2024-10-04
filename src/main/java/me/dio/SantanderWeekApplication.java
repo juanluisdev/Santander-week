60dd4fd2a720319424ade1ec6e3d3fb8e07d722c
juanluisdev <juanluissilveiradeoliveira@gmail.com>
2024-10-04 16:01:56 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderWeekApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderWeekApplication.class, args);
+	}
+
+}
